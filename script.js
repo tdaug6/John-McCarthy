0fe5c2319676a4c16e68e9629cc7372c015c031d
tdaug6 <57757881+tdaug6@users.noreply.github.com>
2019-12-19 08:30:54 -0500

    Updated website and created quiz

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var question = "What year did John McCarthy first suggest an artificial intelligence summer conference?"
-document.getElementById("question1").innerHTML = question;
+var question = "When was the idea of Artificial first suggested"
+document.getElementById("question").innerHTML = question;
 
 var answers = ['1955','1956','1958','1959'];
 
-var answerButtons = document.getElementsByClassName("quizButton1");
+var answerButtons = document.getElementsByClassName("quizButton");
 
 var correct= 0 //The first answer is at position 0, the last is at 3.
 
@@ -19,10 +19,6 @@ for(var spot=0; spot<answers.length; spot++)
             this.setAttribute('style','background-color:rgb(210,50,35);')
         }
     });
-}
-
-function changeColor(){
-    document.getElementById('demo2').setAttribute('style','background-color:black;')
 }
 
 function disableAll(){
